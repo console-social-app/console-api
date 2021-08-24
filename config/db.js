7da0ff6904ce5f728977c1ec21e10a73e26a34a2
adamdeguire <adeguire443@gmail.com>
2021-08-24 10:17:54 -0500

    Configure database

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'project-api'
+const mongooseBaseName = 'socialcluster'
 
 // create the mongodb uri for development and test
 const database = {
