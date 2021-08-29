ff47693a83b83822005ff9902d78bfab7f46b049
ans9611 <ans9611@gmail.com>
2021-08-28 22:07:09 -0400

    fixed sign-up cURL script

--- curl-scripts/auth/sign-in.sh
+++ curl-scripts/auth/sign-in.sh
@@ -10,7 +10,7 @@ curl "${API}${URL_PATH}" \
   --data '{
     "credentials": {
       "email": "'"${EMAIL}"'",
-      "password": "'"${PASSWORD}"'"
+      "password": "'"${PASSWORD}"'",
     }
   }'
 
